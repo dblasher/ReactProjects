cf4557ff05dbf17d5b1584cd4d83ee17f0e3a9d6
Daniel Blasher <dblasher90@gmail.com>
2018-06-09 18:18:35 -0700

    Add files via upload

---
+++ TicTacToe/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+
+//nothing in this file needs to change for Tic Tac Toe
